684439d5867faf0c424f0ddd72c4cce7beb57072
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2016-05-25 02:48:09 +0900

    add comments

--- Tars/distribution.py
+++ Tars/distribution.py
@@ -33,6 +33,10 @@ class Distribution(object):
 
 
 class Bernoulli(Distribution):
+    """
+    Bernoulli distribution
+    p(x) = mean^x * (1-mean)^(1-x)
+    """
 
     def __init__(self, mean_network, given):
         super(Bernoulli, self).__init__(mean_network, given)
@@ -49,23 +53,32 @@ class Bernoulli(Distribution):
         return self.mean_sum_samples(loglike)
 
     def sample_given_x(self, x, srng, deterministic=False):
-        # outputs : [x,z]
+        """
+        inputs : x
+        outputs : [x,z]
+        """
         mean = self.fprop(x, deterministic=deterministic)
-        return [x,self.sample(mean, srng)]
+        return [x, self.sample(mean, srng)]
 
     def sample_mean_given_x(self, x, srng=None, deterministic=False):
-        # outputs : [x,z]
+        """
+        inputs : x
+        outputs : [x,z]
+        """
         mean = self.fprop(x, deterministic=deterministic)
-        return [x,mean]
+        return [x, mean]
 
     def log_likelihood_given_x(self, samples, deterministic=False):
-        # inputs : [x, sample]
         x, sample = samples
         mean = self.fprop(x, deterministic=deterministic)
         return self.log_likelihood(sample, mean)
 
 
 class Categorical(Bernoulli):
+    """
+    Categorical distribution
+    p(x) = \prod mean^x
+    """
 
     def __init__(self, mean_network, given):
         super(Categorical, self).__init__(mean_network, given)
@@ -76,6 +89,10 @@ class Categorical(Bernoulli):
 
 
 class Gaussian(Distribution):
+    """
+    Gaussian distribution
+    p(x) = \frac{1}{\sqrt{2*\pi*var}} * exp{-\frac{{x-mean}^2}{2*var}}
+    """
 
     def __init__(self, mean_network, var_network, given):
         super(Gaussian, self).__init__(mean_network, given)
@@ -102,21 +119,32 @@ class Gaussian(Distribution):
         return self.mean_sum_samples(loglike)
 
     def sample_given_x(self, x, srng, deterministic=False):
+        """
+        inputs : x
+        outputs : [x,z]
+        """
         mean, var = self.fprop(x, deterministic=deterministic)
         return [x, self.sample(mean, var, srng)]
 
     def sample_mean_given_x(self, x, srng=None, deterministic=False):
+        """
+        inputs : x
+        outputs : [x,z]
+        """
         mean, _ = self.fprop(x, deterministic=deterministic)
         return [x, mean]
 
     def log_likelihood_given_x(self, samples, deterministic=False):
-        # inputs : [x, sample]
         x, sample = samples
         mean, var = self.fprop(x, deterministic=deterministic)
         return self.log_likelihood(sample, mean, var)
 
 
 class Gaussian_nonvar(Bernoulli):
+    """
+    Gaussian distribution (no variance)
+    p(x) = \frac{1}{\sqrt{2*\pi}} * exp{-\frac{{x-mean}^2}{2}}
+    """
 
     def __init__(self, mean_network, given):
         super(Gaussian_nonvar, self).__init__(mean_network, given)
@@ -127,6 +155,10 @@ class Gaussian_nonvar(Bernoulli):
 
 
 class UnitGaussian(Distribution):
+    """
+    Standard normal gaussian distribution
+    p(x) = \frac{1}{\sqrt{2*\pi}} * exp{-\frac{x^2}{2}}
+    """
 
     def __init__(self):
         pass
@@ -141,7 +173,12 @@ class UnitGaussian(Distribution):
 
 
 # TODO: conplicated conditional version
-class Multilayer_distribution(object):
+class Multilayer(object):
+    """
+    Multiple layer distribution
+    p(x|z) = p(x|z1)p(z1|z2)...p(zn-1|zn)
+    q(z|x) = q(zn|zn-1)...q(z2|z1)q(z1|x)
+    """
 
     def __init__(self, distributions):
         self.distributions = distributions
@@ -154,33 +191,51 @@ class Multilayer_distribution(object):
         return params
 
     def __sampling(self, x, srng, deterministic):
+        """
+        inputs : x
+        outputs : [x,z1,...,zn-1]
+        """
         samples = [x]
         for i, d in enumerate(self.distributions[:-1]):
             sample = d.sample_given_x(samples[i], srng, deterministic=deterministic)
-            samples.append(sample)
+            samples.append(sample[-1])
         return samples
 
     def fprop(self, x, srng, deterministic=False):
-        samples = __sampling(x, srng, deterministic)
-        sample = self.distributions[-1].fprop(inputs[-1], deterministic=deterministic)
+        """
+        inputs : x
+        outputs : mean
+        """
+        samples = self.__sampling(x, srng, deterministic)
+        sample = self.distributions[-1].fprop(samples[-1:], deterministic=deterministic)
         return sample
 
     def sample_given_x(self, x, srng, deterministic=False):
-        samples = __sampling(x, srng, deterministic)        
-        sample = self.distributions[-1].sample_given_x(samples[-1], deterministic=deterministic)
-        return sample
+        """
+        inputs : x
+        outputs : [x,z1,...,zn]
+        """
+        samples = self.__sampling(x, srng, deterministic)        
+        samples += self.distributions[-1].sample_given_x(samples[-1:], srng, deterministic=deterministic)[-1:]
+        return samples
 
     def sample_mean_given_x(self, x, srng, deterministic=False):
-        samples = __sampling(x, srng, deterministic)        
-        mean = self.distributions[-1].sample_mean_given_x(samples[-1], deterministic=deterministic)
+        """
+        inputs : x
+        outputs : [x,z1,...,zn]
+        """
+        mean = self.__sampling(x, srng, deterministic)
+        mean += self.distributions[-1].sample_mean_given_x(mean[-1:], deterministic=deterministic)[-1:]
         return mean
 
     def log_likelihood_given_x(self, samples, deterministic=False):
-        # inputs : [[x,y,...],z1,z2,...,zn] or [[zn,y,...],zn-1,...,x]
-        # log_likelihood (q) : [q(z1|[x,y,...]),...,q(zn|zn-1)]
-        # log_likelihood (p) : [p(zn-1|[zn,y,...]),...,p(x|z1)]
+        """
+        inputs : [[x,y,...],z1,z2,...,zn] or [[zn,y,...],zn-1,...,x]
+        log_likelihood (q) : [q(z1|[x,y,...]),...,q(zn|zn-1)]
+        log_likelihood (p) : [p(zn-1|[zn,y,...]),...,p(x|z1)]
+        """
         all_log_likelihood = 0
         for x, sample, d in zip(samples, samples[1:], self.distributions):
-            log_likelihood = self.distributions.log_likelihood_given_x([tolist(x),sample])
+            log_likelihood = d.log_likelihood_given_x([tolist(x),sample])
             all_log_likelihood += log_likelihood
         return all_log_likelihood
