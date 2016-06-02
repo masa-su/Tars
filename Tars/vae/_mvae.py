b1c73003edbc4e13d19029f479d72e02636743a8
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2016-06-02 23:14:52 +0900

    delete penalty_train

--- Tars/vae/_mvae.py
+++ Tars/vae/_mvae.py
@@ -5,7 +5,7 @@ import theano.tensor as T
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 from progressbar import ProgressBar
-from ..util import t_repeat, LogMeanExp
+from ..util import t_repeat, LogMeanExp, tolist
 from ..distribution import UnitGaussian
 from copy import copy
 
@@ -56,23 +56,7 @@ class MVAE(VAE):
         updates = self.optimizer(loss, params)
         self.lowerbound_train = theano.function(
             inputs=x, outputs=lowerbound, updates=updates, on_unused_input='ignore')
-        """
-        params = q_params + p0_params + p1_params
-        lowerbound = [KL, loglike0, loglike1, KL_0, KL_1]
-        loss = -np.sum(lowerbound[:3])
 
-        updates = self.optimizer(loss, params)
-        self.lowerbound_train = theano.function(
-            inputs=x, outputs=lowerbound, updates=updates, on_unused_input='ignore')
-
-        params = pq0_params + pq1_params
-        lowerbound = [KL, loglike0, loglike1, KL_0, KL_1]
-        loss = np.sum(lowerbound[3:])
-
-        updates = self.optimizer(loss, params)
-        self.penalty_train = theano.function(
-            inputs=x, outputs=lowerbound, updates=updates, on_unused_input='ignore')
-        """
     def train(self, train_set):
         N = train_set[0].shape[0]
         nbatches = N // self.n_batch
@@ -84,7 +68,6 @@ class MVAE(VAE):
 
             x = [_x[start:end] for _x in train_set]
             train_L = self.lowerbound_train(*x)
-            #train_L = self.penalty_train(*x)
             lowerbound_train.append(np.array(train_L))
         lowerbound_train = np.mean(lowerbound_train, axis=0)
 
@@ -119,6 +102,10 @@ class MVAE(VAE):
         self.pq0_sample_x = theano.function(
             inputs=x, outputs=samples[-1], on_unused_input='ignore')
 
+        samples = self.pq[0].fprop(x, self.srng, deterministic=True)
+        self.pq0_sample_meanvar_x = theano.function(
+            inputs=x, outputs=samples, on_unused_input='ignore')
+
         x = self.pq[1].inputs
         samples = self.pq[1].sample_mean_given_x(x, deterministic=True)
         self.pq1_sample_mean_x = theano.function(
@@ -128,9 +115,13 @@ class MVAE(VAE):
         self.pq1_sample_x = theano.function(
             inputs=x, outputs=samples[-1], on_unused_input='ignore')
 
+        samples = self.pq[1].fprop(x, self.srng, deterministic=True)
+        self.pq1_sample_meanvar_x = theano.function(
+            inputs=x, outputs=samples, on_unused_input='ignore')
+
     def log_importance_weight(self, samples):
         """
-        inputs : [[x0,x1],z1,z2,...,zn]
+        inputs : [[x1],z1,z2,...,zn]
         outputs : log p(x0,x1,z1,z2,...,zn)/q(z1,z2,...,zn|x0,x1)
         """
         log_iw = 0
@@ -156,6 +147,133 @@ class MVAE(VAE):
         p1_log_likelihood = self.p[1].log_likelihood_given_x(inverse_samples1)
 
         log_iw += p0_log_likelihood + p1_log_likelihood - q_log_likelihood
+        log_iw += self.prior.log_likelihood(samples[-1])
+
+        return log_iw
+
+    def log_conditional_likelihood_test(self, test_set, l=1, k=1, mode='iw'):
+        x = self.q.inputs
+        log_likelihood = self.log_conditional_likelihood_iwae(x, k)
+        get_log_likelihood = theano.function(
+            inputs=x, outputs=log_likelihood, on_unused_input='ignore')
+
+        print "start sampling"
+
+        N = test_set[0].shape[0]
+        nbatches = N // self.n_batch
+
+        pbar = ProgressBar(maxval=nbatches).start()
+        all_log_likelihood = []
+        for i in range(nbatches):
+            start = i * self.n_batch
+            end = start + self.n_batch
+            x = [_x[start:end] for _x in test_set]
+            log_likelihood = get_log_likelihood(*x)
+            all_log_likelihood = np.r_[all_log_likelihood, log_likelihood]
+            pbar.update(i)
+
+        return all_log_likelihood
+
+    def log_conditional_likelihood_iwae(self, x, k):
+        n_x = x[0].shape[0]
+        rep_x = [t_repeat(_x, k, axis=0) for _x in x]
+        samples = self.q.sample_given_x(rep_x, self.srng)
+
+        samples = self.single_input(samples,input=rep_x)
+        log_iw = self.log_conditional_importance_weight(samples)
+        log_iw_matrix = T.reshape(log_iw, (n_x, k))
+        log_marginal_estimate = LogMeanExp(
+            log_iw_matrix, axis=1, keepdims=True)
+
+        return log_marginal_estimate
+
+    def log_conditional_importance_weight(self, samples):
+        """
+        inputs : [[x0,x1],z1,z2,...,zn]
+        outputs : log p(x0|z1,z2,...,zn)q(z|y)/q(z|x,y)
+        """
+
+        """
+        log q(z1,z2,...,zn|x0,x1)
+        samples : [[x0,x1],z1,z2,...,zn]
+        """
+        q_log_likelihood = self.q.log_likelihood_given_x(samples)
+
+        """
+        log q(z1,z2,...,zn|x1)
+        samples : [x1,z1,z2,...,zn]
+        """
+        samples1 = self.single_input(samples,1)
+        q1_log_likelihood = self.pq[1].log_likelihood_given_x(samples1)
+
+        """
+        log p(x0|z1,z2,...,zn)
+        inverse_samples0 : [zn,zn-1,...,x0]
+        """
+        inverse_samples0 = self.inverse_samples(self.single_input(samples,0))
+        p0_log_likelihood = self.p[0].log_likelihood_given_x(inverse_samples0)
+
+        log_iw = p0_log_likelihood + q1_log_likelihood - q_log_likelihood
+
+        return log_iw
+
+    def log_mg_likelihood_test(self, test_set, l=1, k=1, mode='iw'):
+        x = self.q.inputs
+        log_likelihood = self.log_mg_likelihood_iwae(x, k)
+        get_log_likelihood = theano.function(
+            inputs=x, outputs=log_likelihood, on_unused_input='ignore')
+
+        print "start sampling"
+
+        N = test_set[0].shape[0]
+        nbatches = N // self.n_batch
+
+        pbar = ProgressBar(maxval=nbatches).start()
+        all_log_likelihood = []
+        for i in range(nbatches):
+            start = i * self.n_batch
+            end = start + self.n_batch
+            x = [_x[start:end] for _x in test_set]
+            log_likelihood = get_log_likelihood(*x)
+            all_log_likelihood = np.r_[all_log_likelihood, log_likelihood]
+            pbar.update(i)
+
+        return all_log_likelihood
+
+    def log_mg_likelihood_iwae(self, x, k):
+        n_x = x[0].shape[0]
+        rep_x = [t_repeat(_x, k, axis=0) for _x in x]
+        samples = self.q.sample_given_x(rep_x, self.srng)
+
+        samples = self.single_input(samples,input=rep_x)
+        log_iw = self.log_mg_importance_weight(samples)
+        log_iw_matrix = T.reshape(log_iw, (n_x, k))
+        log_marginal_estimate = LogMeanExp(
+            log_iw_matrix, axis=1, keepdims=True)
+
+        return log_marginal_estimate
+
+    def log_mg_importance_weight(self, samples):
+        """
+        inputs : [[x0,x1],z1,z2,...,zn]
+        outputs : log p(x0|z1,z2,...,zn)p(z)/q(z|x,y)
+        """
+        log_iw = 0
+
+        """
+        log q(z1,z2,...,zn|x0,x1)
+        samples : [[x0,x1],z1,z2,...,zn]
+        """
+        q_log_likelihood = self.q.log_likelihood_given_x(samples)
+
+        """
+        log p(x0|z1,z2,...,zn)
+        inverse_samples0 : [zn,zn-1,...,x0]
+        """
+        inverse_samples0 = self.inverse_samples(self.single_input(samples,0))
+        p0_log_likelihood = self.p[0].log_likelihood_given_x(inverse_samples0)
+
+        log_iw += p0_log_likelihood - q_log_likelihood
         log_iw += self.prior.log_likelihood(samples[-1])
 
         return log_iw
@@ -205,7 +323,7 @@ class MVAE(VAE):
         """
         _samples = copy(samples)
         if input:
-            _samples[0] = [input]
+            _samples[0] = tolist(input)
         else:
             _samples[0] = [_samples[0][i]]
         return _samples
