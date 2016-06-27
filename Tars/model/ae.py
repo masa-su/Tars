800b83e519032ffca3f96bb4396f6caf6dd5c9b1
Masahiro Suzuki <masa@weblab.t.u-tokyo.ac.jp>
2016-06-27 23:02:56 +0900

    remove UnitGaussian

--- Tars/model/ae.py
+++ Tars/model/ae.py
@@ -2,7 +2,6 @@ import numpy as np
 import theano
 import theano.tensor as T
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
-from ..distribution import UnitGaussian
 from progressbar import ProgressBar
 
 
