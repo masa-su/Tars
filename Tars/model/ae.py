09bdefb1545396a9ae475edef30366bdc2aa0971
Masahiro Suzuki <masa@weblab.t.u-tokyo.ac.jp>
2016-06-27 21:34:49 +0900

    add import RandomStreams

--- Tars/model/ae.py
+++ Tars/model/ae.py
@@ -1,6 +1,7 @@
 import numpy as np
 import theano
 import theano.tensor as T
+from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 from ..distribution import UnitGaussian
 from progressbar import ProgressBar
 
