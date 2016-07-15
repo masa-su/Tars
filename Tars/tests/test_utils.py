3efbd6cbc506329f5cc9e6eb9ea5b6bc07ea9024
ysekky <s.yoshi.mobile.1123@gmail.com>
2016-07-15 17:36:33 +0900

    kerasからコピペしてやってみる

--- Tars/tests/test_utils.py
+++ Tars/tests/test_utils.py
@@ -1,8 +1,8 @@
-import unittest
+from unittest import TestCase
 import mock
 
 
-class TestEpsilon(unittest.TestCase):
+class TestEpsilon(TestCase):
 
     def test_it(self):
         from ..utils import epsilon, set_epsilon
@@ -12,3 +12,13 @@ class TestEpsilon(unittest.TestCase):
             new_eps = 30
             set_epsilon(new_eps)
             self.assertEquals(epsilon(), new_eps)
+
+
+class TestToList(TestCase):
+
+    def test_it(self):
+        from ..utils import tolist
+        sample_list = [1, 2, 3, 4]
+        self.assertEqual(sample_list, tolist(sample_list))
+        sample_value = 1
+        self.assertEqual([sample_value], tolist(sample_value))
