d3e12f5496c49f20d9aba7334ce3a83835335132
ysekky <s.yoshi.mobile.1123@gmail.com>
2016-07-15 20:12:10 +0900

    add test

--- Tars/tests/test_utils.py
+++ Tars/tests/test_utils.py
@@ -1,5 +1,8 @@
 from unittest import TestCase
+
 import mock
+import numpy as np
+import theano.tensor as T
 
 
 class TestEpsilon(TestCase):
@@ -22,3 +25,39 @@ class TestToList(TestCase):
         self.assertEqual(sample_list, tolist(sample_list))
         sample_value = 1
         self.assertEqual([sample_value], tolist(sample_value))
+
+
+class TestLogMeanExp(TestCase):
+
+    # https://github.com/blei-lab/edward/blob/c584c423251316a227fbfb2f669aaf45ac236c40/tests/test_log_mean_exp.py
+    def test_1d(self):
+        from ..utils import log_mean_exp
+        x = T.constant([-1.0, -2.0, -3.0, -4.0])
+        val_ed = log_mean_exp(x)
+        val_true = -1.9461046625586951
+        self.assert_(np.allclose(val_ed.eval(), val_true))
+
+    def test_2d(self):
+        from ..utils import log_mean_exp
+        x = T.constant([[-1.0], [-2.0], [-3.0], [-4.0]])
+        val_ed = log_mean_exp(x)
+        val_true = -1.9461046625586951
+        self.assert_(np.allclose(val_ed.eval(), val_true))
+
+
+class TestLogSumExp(TestCase):
+
+    # https://github.com/blei-lab/edward/blob/c584c423251316a227fbfb2f669aaf45ac236c40/tests/test_log_sum_exp.py
+    def test_1d(self):
+        from ..utils import log_sum_exp
+        x = T.constant([-1.0, -2.0, -3.0, -4.0])
+        val_ed = log_sum_exp(x)
+        val_true = -0.5598103014388045
+        assert np.allclose(val_ed.eval(), val_true)
+
+    def test_2d(self):
+        from ..utils import log_sum_exp
+        x = T.constant([[-1.0], [-2.0], [-3.0], [-4.0]])
+        val_ed = log_sum_exp(x)
+        val_true = -0.5598103014388045
+        assert np.allclose(val_ed.eval(), val_true)
