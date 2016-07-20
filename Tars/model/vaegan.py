4688f6b569431e24c69c68a969bc9fe1166d4e5a
ysekky <s.yoshi.mobile.1123@gmail.com>
2016-07-20 22:41:28 +0900

    vaegan

--- Tars/model/vaegan.py
+++ Tars/model/vaegan.py
@@ -15,21 +15,34 @@ from ..utils import (
 
 class VAEGAN(VAE, GAN):
 
-    def __init__(self, q, p, d, n_batch, optimizer, l=1, k=1, gamma=1, random=1234):
+    def __init__(self, q, p, d, n_batch, optimizer,
+                 l=1, k=1, gamma=1, random=1234):
         self.d = d
         self.gamma = gamma
 
-        super(VAEGAN, self).__init__(q, p, n_batch, optimizer, l, k, alpha=None, random=random)
+        super(VAEGAN, self).__init__(q, p, n_batch, optimizer,
+                                     l, k, alpha=None, random=random)
 
     def loss(self, gz, x, deterministic=False):
         p_loss, d_loss = super(VAEGAN, self).loss(gz, x, deterministic)
 
-        z = self.q.sample_given_x(x, self.srng, deterministic=deterministic)[-1]
-        rec_x = self.p.sample_mean_given_x([z]+x[1:], deterministic=deterministic)[-1]
-        rec_t = self.d.sample_mean_given_x([rec_x]+x[1:], deterministic=deterministic)[-1] # rec_t~d(rec_t|rec_x,y,...)
-
-        rec_d_g_loss = -self.d.log_likelihood(T.zeros_like(rec_t),rec_t).mean() # -log(1-rec_t)
-        rec_p_loss = -self.d.log_likelihood(T.ones_like(rec_t),rec_t).mean() # -log(rec_t)
+        z = self.q.sample_given_x(
+            x,
+            self.srng,
+            deterministic=deterministic)[-1]
+        rec_x = self.p.sample_mean_given_x(
+            [z] + x[1:],
+            deterministic=deterministic)[-1]
+        # rec_t~d(rec_t|rec_x,y,...)
+        rec_t = self.d.sample_mean_given_x(
+            [rec_x] + x[1:],
+            deterministic=deterministic)[-1]
+        # -log(1-rec_t)
+        rec_d_g_loss = -self.d.log_likelihood(
+            T.zeros_like(rec_t), rec_t).mean()
+        # -log(rec_t)
+        rec_p_loss = -self.d.log_likelihood(
+            T.ones_like(rec_t), rec_t).mean()
         p_loss = p_loss + rec_p_loss
         d_loss = d_loss + rec_d_g_loss
         return p_loss, d_loss
@@ -48,26 +61,48 @@ class VAEGAN(VAE, GAN):
 
         # ---GAN---
         gz = self.p.inputs
-        p_loss, d_loss = self.loss(gz,x,False)
+        p_loss, d_loss = self.loss(gz, x, False)
 
         lowerbound = [-KL, loglike, p_loss, d_loss]
 
         q_params = self.q.get_params()
         p_params = self.p.get_params()
         d_params = self.d.get_params()
-        q_updates = self.optimizer(KL -loglike, q_params, learning_rate=1e-4, beta1=0.5)
-        p_updates = self.optimizer(-self.gamma*loglike + p_loss, p_params, learning_rate=1e-4, beta1=0.5)
-        d_updates = self.optimizer(d_loss, d_params, learning_rate=1e-4, beta1=0.5)
+        q_updates = self.optimizer(
+            KL - loglike, q_params,
+            learning_rate=1e-4,
+            beta1=0.5)
+        p_updates = self.optimizer(
+            -self.gamma * loglike + p_loss,
+            p_params,
+            learning_rate=1e-4,
+            beta1=0.5)
+        d_updates = self.optimizer(
+            d_loss,
+            d_params,
+            learning_rate=1e-4,
+            beta1=0.5)
 
         self.q_lowerbound_train = theano.function(
-            inputs=gz[:1]+x, outputs=lowerbound, updates=q_updates, on_unused_input='ignore')
+            inputs=gz[:1]+x,
+            outputs=lowerbound,
+            updates=q_updates,
+            on_unused_input='ignore')
         self.p_lowerbound_train = theano.function(
-            inputs=gz[:1]+x, outputs=lowerbound, updates=p_updates, on_unused_input='ignore')
+            inputs=gz[:1]+x,
+            outputs=lowerbound,
+            updates=p_updates, on_unused_input='ignore')
         self.d_lowerbound_train = theano.function(
-            inputs=gz[:1]+x, outputs=lowerbound, updates=d_updates, on_unused_input='ignore')
+            inputs=gz[:1]+x,
+            outputs=lowerbound,
+            updates=d_updates,
+            on_unused_input='ignore')
 
         p_loss, d_loss = self.loss(gz, x, True)
-        self.test = theano.function(inputs=gz[:1]+x, outputs=[p_loss,d_loss], on_unused_input='ignore')
+        self.test = theano.function(
+            inputs=gz[:1]+x,
+            outputs=[p_loss, d_loss],
+            on_unused_input='ignore')
 
     def train(self, train_set, z_dim, rng):
         n_x = train_set[0].shape[0]
@@ -80,7 +115,10 @@ class VAEGAN(VAE, GAN):
             end = start + self.n_batch
 
             batch_x = [_x[start:end] for _x in train_set]
-            batch_z = rng.uniform(-1., 1., size=(len(batch_x[0]), z_dim)).astype(np.float32)
+            batch_z = rng.uniform(-1.,
+                                  1.,
+                                  size=(len(batch_x[0]), z_dim)
+                                  ).astype(np.float32)
             batch_zx = [batch_z]+batch_x
 
             train_L = self.q_lowerbound_train(*batch_zx)
