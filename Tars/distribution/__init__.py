fbab8ec1d452674d36e7ac338a2ac0a3980a0c9f
ysekky <s.yoshi.mobile.1123@gmail.com>
2016-07-22 16:30:12 +0900

    remove * import and add __all__

--- Tars/distribution/__init__.py
+++ Tars/distribution/__init__.py
@@ -1,2 +1,30 @@
-from .exponential_family import *
-from .multiple import *
+from .exponential_family import (
+    Distribution,
+    Deterministic,
+    Bernoulli,
+    Categorical,
+    Gaussian,
+    BivariateGauss,
+    GaussianConstantVar,
+    UnitGaussian,
+    Laplace,
+)
+from .multiple import (
+    Concatenate,
+    Multilayer,
+)
+
+
+__all__ = [
+    'Distribution',
+    'Deterministic',
+    'Bernoulli',
+    'Categorical',
+    'Gaussian',
+    'BivariateGauss',
+    'GaussianConstantVar',
+    'UnitGaussian',
+    'Laplace',
+    'Concatenate',
+    'Multilayer',
+]
