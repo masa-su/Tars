a95d220e82d2450a1b19cb80f26893084bc732d8
Masahiro Suzuki <masa@weblab.t.u-tokyo.ac.jp>
2016-08-18 17:26:07 +0900

    add some docstrings

--- Tars/distribution/multiple.py
+++ Tars/distribution/multiple.py
@@ -100,17 +100,44 @@ class Concatenate(object):
         return loglikes
 
 
-# TODO: conplicated conditional version
 class Multilayer(object):
     """
-    Multiple layer distribution
-    p(x|z) = p(x|z1)p(z1|z2)...p(zn-1|zn)
-    q(z|x) = q(zn|zn-1)...q(z2|z1)q(z1|x)
+    This distribution is used to stack multiple distiributions, that is
+    p(x|z) = p(x|z1)p(z1|z2)...p(zn-1|zn). If the distributions are
+    approximate distributions, then a corresponding stacked distribution
+    becomes like q(z|x) = q(zn|zn-1)...q(z2|z1)q(z1|x). If the stacked
+    distribution is conditioned on y, then the corresponding mean field
+    approximation becomes like p(x|z,y) = p(x|z1)p(z1|z2)...p(zn-1|zn,y),
+    or q(z|x,y) = q(zn|zn-1)...q(z2|z1)q(z1|x,y).
+    So far, each distribution except first layer cannot have conditioned
+    variables more than two.
+
+    Samples
+    -------
+    distributions : list
+       Contain multiple distributions.
+
+    Examples
+    --------
+    >>> from Tars.distribution import Multilayer, Gaussian, Bernoulli
+    >>> gauss = Gaussian(mean, var, given=[x])
+    >>> bernoulli = Bernoulli(mean, given=[z])
+    >>> multi = Multilayer([gauss, bernoulli])
     """
 
     def __init__(self, distributions):
         self.distributions = distributions
         self.inputs = self.distributions[0].inputs
+        for i, d in enumerate(distributions[1:]):
+            if len(d.given) != 1:
+                raise ValueError("So far, each distribution except first"
+                                 "layer cannot have conditioned variables"
+                                 "more than two.")
+
+            if distributions[i].get_output_shape != d.given[0].shape:
+                raise ValueError("An output's shape of a distribution must be"
+                                 "same as an input's shape of the next layer"
+                                 "distribution.")
 
     def get_params(self):
         params = []
@@ -118,55 +145,106 @@ class Multilayer(object):
             params += d.get_params()
         return params
 
-    def _sampling(self, x, srng, deterministic):
+    def _sampling(self, x, srng, **kwargs):
         """
-        inputs : x
-        outputs : [x,z1,...,zn-1]
+        Paramaters
+        ----------
+        x : list
+           This contains Theano variables.
+
+        srng : theano.sandbox.MRG_RandomStreams
+
+        Returns
+        -------
+        list
+           This contains 'x' and samples, such as [x,z1,...,zn-1].
         """
+
         samples = [x]
         for i, d in enumerate(self.distributions[:-1]):
             sample = d.sample_given_x(
-                samples[i], srng, deterministic=deterministic)
+                samples[i], srng, **kwargs)
             samples.append(sample[-1])
         return samples
 
-    def fprop(self, x, srng, deterministic=False):
+    def fprop(self, x, srng, **kwargs):
         """
-        inputs : x
-        outputs : mean
-        """
-        samples = self._sampling(x, srng, deterministic)
-        sample = self.distributions[-1].fprop(
-            tolist(samples[-1]), deterministic=deterministic)
-        return sample
+        Paramaters
+        ----------
+        x : list
+           This contains Theano variables.
 
-    def sample_given_x(self, x, srng, deterministic=False):
+        srng : theano.sandbox.MRG_RandomStreams
+
+        Returns
+        -------
+        mean : Theano variable
+            The output of this distribution.
         """
-        inputs : x
-        outputs : [x,z1,...,zn]
+
+        samples = self._sampling(x, srng, **kwargs)
+        mean = self.distributions[-1].fprop(
+            tolist(samples[-1]), **kwargs)
+        return mean
+
+    def sample_given_x(self, x, srng, **kwargs):
         """
-        samples = self._sampling(x, srng, deterministic)
+        Paramaters
+        --------
+        x : list
+           This contains Theano variables, which must to correspond
+           to 'given' of first layer distibution.
+
+        srng : theano.sandbox.MRG_RandomStreams
+
+        Returns
+        --------
+        list
+           This contains 'x' and samples, such as [x,z1,...,zn].
+        """
+
+        samples = self._sampling(x, srng, **kwargs)
         samples += self.distributions[-1].sample_given_x(
-            tolist(samples[-1]), srng, deterministic=deterministic)[-1:]
+            tolist(samples[-1]), srng, **kwargs)[-1:]
         return samples
 
-    def sample_mean_given_x(self, x, srng, deterministic=False):
+    def sample_mean_given_x(self, x, srng, **kwargs):
         """
-        inputs : x
-        outputs : [x,z1,...,zn]
+        Paramaters
+        --------
+        x : list
+           This contains Theano variables, which must to correspond
+           to 'given'.
+
+        Returns
+        --------
+        list
+           This contains 'x', samples, and a mean value of sample,
+           such as [x,z1,...,zn_mean]
         """
-        mean = self._sampling(x, srng, deterministic)
+
+        mean = self._sampling(x, srng, **kwargs)
         mean += self.distributions[-1].sample_mean_given_x(
-            tolist(mean[-1]), deterministic=deterministic)[-1:]
+            tolist(mean[-1]), **kwargs)[-1:]
         return mean
 
     def log_likelihood_given_x(self, samples, deterministic=False):
         """
-        inputs : [[x,y,...],z1,z2,...,zn] or [[zn,y,...],zn-1,...,x]
-        outputs :
-           log_likelihood (q) : [q(z1|[x,y,...]),...,q(zn|zn-1)]
-           log_likelihood (p) : [p(zn-1|[zn,y,...]),...,p(x|z1)]
+        Paramaters
+        --------
+        samples : list
+           This contains 'x', which has Theano variables, and test samples,
+           such as z1, z2,...,zn.
+
+        deterministic : bool
+
+        Returns
+        --------
+        Theano variable, shape (n_samples,)
+           log_likelihood (q) : log_q(z1|[x,y,...])+...+log_q(zn|zn-1)
+           log_likelihood (p) : log_p(zn-1|[zn,y,...])+...+log_p(x|z1)
         """
+
         all_log_likelihood = 0
         for x, sample, d in zip(samples, samples[1:], self.distributions):
             log_likelihood = d.log_likelihood_given_x([tolist(x), sample])
