e894a9450ff68a4a2f0ef265640718fd109f9739
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2016-09-06 20:20:57 +0900

    add import LSTMCell

--- Tars/layer/__init__.py
+++ Tars/layer/__init__.py
@@ -1,10 +1,12 @@
 from .recurrent import GRUCell
+from .recurrent import LSTMCell
 from .conv_recurrent import ConvLSTMCell
 
 from .shape import RepeatLayer
 
 __all__ = [
     "GRUCell",
+    "LSTMCell",
     "ConvLSTMCell",
     "RepeatLayer",
 ]
