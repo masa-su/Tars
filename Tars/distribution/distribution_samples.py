40e41bce28de4070f3e76b197cf3d32a0b914883
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2016-11-15 18:53:27 +0900

    add sampling of bernoulli and categorical

--- Tars/distribution/distribution_samples.py
+++ Tars/distribution/distribution_samples.py
@@ -59,10 +59,16 @@ class Bernoulli_sample(object):
            i.e. sample ~ p(x|mean)
         """
 
-        if self.temp != 0:
-            return self.concrete.sample(mean, srng)
+        mean = T.stack([mean, 1-mean], axis=-1)
+        output = self.concrete.sample(mean, srng)
+
+        if output.ndim == 2:
+            return output[:, 0]
+        elif output.ndim == 3:
+            return output[:, :, 0]
         else:
-            return srng.binomial(size=mean.shape, p=mean, dtype=mean.dtype)
+            raise ValueError('Input must be 1-d or 2-d tensor. Got %s' %
+                             mean.ndim)
 
     def log_likelihood(self, sample, mean):
         """
@@ -90,14 +96,53 @@ class Bernoulli_sample(object):
         return mean_sum_samples(loglike)
 
 
-class Categorical_sample(Bernoulli_sample):
+class Categorical_sample(object):
     """
     Categorical distribution
     p(x) = \prod mean^x
     """
 
-    def __init__(self, temp=0.1):
-        super(Categorical_sample, self).__init__(temp=temp)
+    def __init__(self, temp=0.1, n_dim=1):
+        self.temp = temp
+        self.concrete = Concrete_sample(temp)
+        self.n_dim = n_dim
+
+    def sample(self, mean, srng, onehot=True, flatten=True):
+        """
+        Paramaters
+        --------
+        mean : Theano variable, the output of a fully connected layer (softmax)
+           The paramater (mean value) of this distribution.
+
+        Returns
+        -------
+        Theano variable, shape (mean.shape)
+           This variable is sampled from this distribution.
+           i.e. sample ~ p(x|mean)
+        """
+
+        if self.n_dim == 1 and (mean.ndim == 1 or mean.ndim == 2):
+            output = self.concrete.sample(mean, srng)
+            if not onehot:
+                output = T.argmax(output, axis=-1)
+            return output
+
+        elif self.n_dim >= 1 and mean.ndim == 3:
+            if mean.shape[1].eval() == self.n_dim:
+                _shape = mean.shape
+                mean = mean.reshape((_shape[0]*_shape[1], _shape[2]))
+                output = self.concrete.sample(mean, srng).reshape(_shape)
+                if not onehot:
+                    output = T.argmax(output, axis=-1)
+                if flatten:
+                    output = T.flatten(output)
+                return output
+
+            raise ValueError('mean.shape[1] is incongruous with n_dim,'
+                             'got mean.shape[1] = %s and n_dim = %s'
+                             % (mean.shape[1].eval(), mean.ndim))
+
+        raise ValueError('Wrong input or n_dim.')
 
     def log_likelihood(self, samples, mean):
         """
@@ -174,7 +219,8 @@ class GaussianConstantVar_sample(Gaussian_sample):
     def log_likelihood(self, samples, mean):
         return super(GaussianConstantVar_sample,
                      self).log_likelihood(samples, mean,
-                                          T.ones_like(samples) * self.constant_var)
+                                          T.ones_like(samples)
+                                          * self.constant_var)
 
 
 class UnitGaussian_sample(Gaussian_sample):
@@ -194,9 +240,10 @@ class UnitGaussian_sample(Gaussian_sample):
         return srng.normal(shape)
 
     def log_likelihood(self, samples):
-        return super(UnitGaussian_sample, self).log_likelihood(samples,
-                                                               T.zeros_like(samples),
-                                                               T.ones_like(samples))
+        return super(UnitGaussian_sample,
+                     self).log_likelihood(samples,
+                                          T.zeros_like(samples),
+                                          T.ones_like(samples))
 
 
 class Laplace_sample(object):
@@ -241,7 +288,7 @@ class Gumbel_sample(object):
 
     def sample(self, mu, beta, srng):
         U = srng.uniform(mu.shape, low=0, high=1, dtype=mu.dtype)
-        return mu - beta * T.log(T.log(U + epsilon()))
+        return mu - beta * T.log(-T.log(U + epsilon()) + epsilon())
 
     def log_likelihood(self, samples, mu, beta):
         """
@@ -264,7 +311,7 @@ class Gumbel_sample(object):
 
 class Concrete_sample(Gumbel_sample):
     """
-    Concrete distribution
+    Concrete distribution (Gumbel-softmax)
         https://arxiv.org/abs/1611.01144
         https://arxiv.org/abs/1611.00712
     """
@@ -273,10 +320,32 @@ class Concrete_sample(Gumbel_sample):
         self.temp = temp
 
     def sample(self, mean, srng):
-        output = super(Concrete_sample, self).sample(T.zeros_like(mean),
-                                                     T.ones_like(mean), srng)
-        output += mean
-        return T.nnet.softmax(output / self.temp)
+        """
+        Paramaters
+        --------
+        sample : Theano variable
+
+        mean : Theano variable, the output of a fully connected layer
+        (sigmoid or softmax)
+        """
+
+        if self.temp != 0:
+            output = super(Concrete_sample, self).sample(T.zeros_like(mean),
+                                                         T.ones_like(mean),
+                                                         srng)
+            output += T.log(mean + epsilon())
+
+            if output.ndim == 1 or output.ndim == 2:
+                return T.nnet.softmax(output / self.temp)
+            elif output.ndim == 3:
+                _shape = output.shape
+                output = output.reshape((_shape[0]*_shape[1], _shape[2]))
+                return T.nnet.softmax(output / self.temp).reshape(_shape)
+
+            raise ValueError('Input must be 1-d, 2-d or 3-d tensor. Got %s' %
+                             output.ndim)
+
+        raise NotImplementedError
 
     def log_likelihood(self):
         raise NotImplementedError
