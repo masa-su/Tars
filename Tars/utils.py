b9231556d8269b65498b8d6f13be32311fb45357
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2016-11-18 15:38:25 +0900

    rm functions

--- Tars/utils.py
+++ Tars/utils.py
@@ -26,27 +26,6 @@ def load_weights(network, name):
     lasagne.layers.set_all_param_values(network, param_values)
 
 
-def gaussian_like(x, mean, var):
-    c = - 0.5 * math.log(2 * math.pi)
-    _var = var + epsilon()  # avoid NaN
-    return c - T.log(_var) / 2 - (x - mean)**2 / (2 * _var)
-
-
-def gauss_unitgauss_kl(mean, var):
-    return -0.5 * T.sum(1 + T.log(var) - mean**2 - var, axis=1)
-
-
-def gauss_gauss_kl(mean1, var1, mean2, var2):
-    _var2 = var2 + epsilon()  # avoid NaN
-    _kl = T.log(var2) - T.log(var1) \
-        + (var1 + (mean1 - mean2)**2) / _var2 - 1
-    return 0.5 * T.sum(_kl, axis=1)
-
-
-def beta(a, b):
-    return T.exp(T.gammaln(a) + T.gammaln(b) - T.gammaln(a + b))
-
-
 # https://github.com/yburda/iwae/blob/master/utils.py
 def t_repeat(x, num_repeats, axis):
     """
