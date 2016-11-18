bfa05f43df650719f87a75ffc158b34936c5bb22
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2016-11-18 16:29:05 +0900

    rm unused models

--- Tars/model/__init__.py
+++ Tars/model/__init__.py
@@ -3,39 +3,8 @@ from .ae import AE
 from .vae import VAE
 from .gan import GAN
 
-from .mvae_old import MVAE_OLD
-from .mvae import MVAE
-
-from .cmma import CMMA
-from .cmmagan import CMMAGAN
-
-from .vaegan import VAEGAN
-from .mvaegan import MVAEGAN
-from .mvaegan_old import MVAEGAN_OLD
-
-from .vae_semi import VAE_semi
-from .vaegan_semi import VAEGAN_semi
-
-from .vrnn import VRNN
-
-from .draw import DRAW
-from .draw_conv import ConvDRAW
-
-
 __all__ = [
     'AE',
     'VAE',
     'GAN',
-    'MVAE',
-    'MVAE_OLD',
-    'CMMA',
-    'CMMAGAN',
-    'VAEGAN',
-    'MVAEGAN',
-    'MVAEGAN_OLD',
-    'VAE_semi',
-    'VAEGAN_semi',
-    'VRNN',
-    'DRAW',
-    'ConvDRAW'
 ]
