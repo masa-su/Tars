601bec760d5ec2c907796c61df8ab1fac28b6413
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2017-02-03 05:03:56 +0900

    add dist_mode in inverse_samples

--- Tars/models/model.py
+++ Tars/models/model.py
@@ -20,15 +20,25 @@ class Model(object):
         self.rng = np.random.RandomState(seed)
         self.srng = RandomStreams(seed)
 
-    def _inverse_samples(self, samples):
+    def _inverse_samples(self, samples, dist_mode="Normal"):
         """
         inputs : [[x,y],z1,z2,...zn]
-        outputs : [[zn,y],zn-1,...x]
+        outputs :
+           if mode is "Normal" : [[zn,y],zn-1,...x], zn
+           elif mode is "MultiPrior" : [z1, x], [[zn,y],zn-1,...z1]
         """
         inverse_samples = samples[::-1]
         inverse_samples[0] = [inverse_samples[0]] + inverse_samples[-1][1:]
         inverse_samples[-1] = inverse_samples[-1][0]
-        return inverse_samples
+
+        if dist_mode == "Normal":
+            return inverse_samples, samples[-1]
+
+        elif dist_mode == "MultiPrior":
+            return inverse_samples[-2:], inverse_samples[:-1]
+
+        raise Exception("You should set dist_mode to 'Normal' or"
+                        "'MultiPrior', got %s." % dist_mode)
 
     def _get_updates(self, loss, params, optimizer, optimizer_params={},
                      clip_grad=None, max_norm_constraint=None):
