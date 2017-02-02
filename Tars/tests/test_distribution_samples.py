810e98be6676b3d4f1c14f9adbf4e876094efab7
pentiumx <pentiumx@live.jp>
2017-02-02 10:37:47 +0900

    Rename local variables

--- Tars/tests/test_distribution_samples.py
+++ Tars/tests/test_distribution_samples.py
@@ -20,7 +20,7 @@ class TestBernoulliSample(TestCase):
         self.bernoulli_sample = Bernoulli_sample(temp=0.01, seed=1)
 
     @staticmethod
-    def get_sample(mean, bernoulli, size):
+    def get_samples(mean, bernoulli, size):
         # get a sample from given bernoulli distribution in ndarray
         mean_sample = np.ones(size).astype("float32") * mean
         t_mean = T.fvector("mean")
@@ -35,7 +35,7 @@ class TestBernoulliSample(TestCase):
         # ref: https://github.com/numpy/numpy/blob/master/numpy/random/tests/test_random.py
         zeros = np.zeros(1000, dtype='float')
         mean = 0
-        samples = TestBernoulliSample.get_sample(mean, self.bernoulli_sample, 1000)
+        samples = TestBernoulliSample.get_samples(mean, self.bernoulli_sample, 1000)
         self.assertTrue(np.allclose(zeros, samples))
 
     def test_mean_one(self):
@@ -44,7 +44,7 @@ class TestBernoulliSample(TestCase):
         # ref: https://github.com/numpy/numpy/blob/master/numpy/random/tests/test_random.py
         ones = np.ones(1000, dtype='float')
         mean = 1
-        samples = TestBernoulliSample.get_sample(mean, self.bernoulli_sample, 1000)
+        samples = TestBernoulliSample.get_samples(mean, self.bernoulli_sample, 1000)
         self.assertTrue(np.allclose(ones, samples))
 
 
@@ -55,14 +55,14 @@ class TestGaussianSample(TestCase):
     @staticmethod
     def get_sample(mean, var, gaussian, size):
         # get a sample from given bernoulli distribution in ndarray
-        mean_sample = np.ones(size).astype("float32") * mean
-        var_sample = np.ones(size).astype("float32") * var
-        t_mean = T.fvector("mean")
+        mean_vector = np.ones(size).astype("float32") * mean
+        var_vector = np.ones(size).astype("float32") * var
+        t_mean = T.fvector("mean") # A theano symbolic variable
         t_var = T.fvector("var")
-        sample = gaussian.sample(t_mean, t_var)
-        f = theano.function(inputs = [t_mean, t_var], outputs=sample)
-        samples = f(mean_sample, var_sample)
-        return samples
+        t_sample = gaussian.sample(t_mean, t_var)
+        f = theano.function(inputs = [t_mean, t_var], outputs=t_sample)
+        sample = f(mean_vector, var_vector)
+        return sample
 
     def test_gaussian(self):
         # Test that Gaussian_sample.sample generates the same result as theano and numpy
@@ -70,18 +70,20 @@ class TestGaussianSample(TestCase):
 
         gaussian_sample = Gaussian_sample(seed=utt.fetch_seed())
         mean, var = 0, 1
-        tars_samples = TestGaussianSample.get_sample(mean, var, gaussian_sample, 5)
+        tars_sample = TestGaussianSample.get_sample(mean, var, gaussian_sample, 5)
 
         random = RandomStreams(utt.fetch_seed())
-        fn = function([], random.normal((5,), 0, 1))
-        theano_samples = fn()
+        fn = function([], random.normal((5,), mean, var))
+        theano_sample = fn()
 
         rng_seed = np.random.RandomState(utt.fetch_seed()).randint(2**30)
         rng = np.random.RandomState(int(rng_seed))  # int() is for 32bit
-        numpy_samples = rng.normal(0, 1, (5,))
+        numpy_sample = rng.normal(mean, var, (5,))
 
-        assert_array_almost_equal(tars_samples, theano_samples, decimal=7)
-        assert_array_almost_equal(tars_samples, numpy_samples, decimal=7)
+        # As Distribution_sample.sample method performs reparametrization trick,
+        # the return value is slightly different from numpy result.
+        assert_array_almost_equal(tars_sample, theano_sample, decimal=7)
+        assert_array_almost_equal(tars_sample, numpy_sample, decimal=7)
 
 
 
