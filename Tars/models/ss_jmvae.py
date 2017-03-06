ffea4a2d5e0f2fd15649f31c8ff543fef9961afc
Masahiro Suzuki <masa@weblab.t.u-tokyo.ac.jp>
2017-03-07 01:12:21 +0900

    add semi-supervised model

--- Tars/models/ss_jmvae.py
+++ Tars/models/ss_jmvae.py
@@ -1,8 +1,9 @@
-from copy import copy
+from copy import copy, deepcopy
 
 import theano
 import theano.tensor as T
 import lasagne
+import numpy as np
 
 from ..utils import tolist, log_mean_exp
 from ..distributions.estimate_kl import analytical_kl
@@ -12,12 +13,17 @@ from . import VAE
 class SS_JMVAE(VAE):
 
     def __init__(self, q, p, f, s_q, prior=None,
-                 n_batch=100, optimizer=lasagne.updates.adam,
+                 n_batch=100, n_batch_u=10000,
+                 discriminate_rate=0.1,
+                 optimizer=lasagne.updates.adam,
                  optimizer_params={},
                  clip_grad=None, max_norm_constraint=None,
-                 train_iw=True, test_iw=True, seed=1234):
+                 seed=1234):
         self.s_q = s_q
         self.f = f
+        self.n_batch_u = n_batch_u
+        self.discriminate_rate = discriminate_rate
+
         super(SS_JMVAE,
               self).__init__(q, p, prior=prior,
                              n_batch=n_batch,
@@ -25,15 +31,136 @@ class SS_JMVAE(VAE):
                              optimizer_params=optimizer_params,
                              clip_grad=clip_grad,
                              max_norm_constraint=max_norm_constraint,
-                             train_iw=train_iw, test_iw=test_iw,
+                             train_iw=True, test_iw=True,
                              iw_alpha=0, seed=seed)
 
+        # set inputs
+        x_u = self.q.inputs
+        x_l = deepcopy(self.q.inputs)
+        y = T.fmatrix("y")
+        l = T.iscalar("l")
+        k = T.iscalar("k")
+
+        # training
+        if self.train_iw:
+            inputs = x_u + x_l + [y, l, k]
+            lower_bound_u, loss_u, params = self._vr_bound(x_u, l, k, 0, False)
+            lower_bound_l, loss_l, _ = self._vr_bound(x_l, l, k, 0, False, tolist(y))
+            lower_bound_y, loss_y = self._discriminate(x_l, tolist(y), False)
+        else:
+            raise NotImplementedError
+
+        rate = self.discriminate_rate * (self.n_batch + self.n_batch_u) / self.n_batch
+        loss = loss_u + loss_l + rate * loss_y
+
+        lower_bound = T.stack([lower_bound_u, lower_bound_l, lower_bound_y], axis=-1)
+        lower_bound = T.mean(lower_bound, axis=0)
+
+        updates = self._get_updates(loss, params, optimizer, optimizer_params,
+                                    clip_grad, max_norm_constraint)
+
+        self.lower_bound_train = theano.function(inputs=inputs,
+                                                 outputs=lower_bound,
+                                                 updates=updates,
+                                                 on_unused_input='ignore')
+
+        # test
+        if self.test_iw:
+            inputs = x_u + x_l + [y, l, k]
+            lower_bound_u, loss_u, params = self._vr_bound(x_u, l, k, 0, True)
+            lower_bound_l, loss_l, _ = self._vr_bound(x_l, l, k, 0, True, tolist(y))
+            lower_bound_y, loss_y = self._discriminate(x_l, tolist(y), True)
+        else:
+            raise NotImplementedError
+
+        lower_bound = T.stack([lower_bound_u, lower_bound_l, lower_bound_y], axis=-1)
+        lower_bound = T.mean(lower_bound, axis=0)
+
+        self.lower_bound_test = theano.function(inputs=inputs,
+                                                outputs=lower_bound,
+                                                on_unused_input='ignore')
+
+    def train(self, train_set_u, train_set_l, l=1, k=1, verbose=False):
+        n_x = train_set_l[0].shape[0]
+        nbatches = n_x // self.n_batch
+        lower_bound_all = []
+
+        if verbose:
+            pbar = ProgressBar(maxval=nbatches).start()
+
+        for i in range(nbatches):
+            # unlabel
+            start = i * self.n_batch_u
+            end = start + self.n_batch_u
+            batch_x_u = [_x[start:end] for _x in train_set_u]
+
+            # label
+            start = i * self.n_batch
+            end = start + self.n_batch
+            batch_x_l = [_x[start:end] for _x in train_set_l]
+
+            _x = batch_x_u + batch_x_l + [l, k]
+            lower_bound = self.lower_bound_train(*_x)
+            lower_bound_all.append(np.array(lower_bound))
+
+            if verbose:
+                pbar.update(i)
+
+        lower_bound_all = np.mean(lower_bound_all, axis=0)
+        return lower_bound_all
+
+    def test(self, test_set_u, test_set_l, l=1, k=1, n_batch=None, verbose=True):
+        if n_batch is None:
+            n_batch = self.n_batch
+
+        n_x = test_set_l[0].shape[0]
+        nbatches = n_x // n_batch
+        lower_bound_all = []
+
+        if verbose:
+            pbar = ProgressBar(maxval=nbatches).start()
+        for i in range(nbatches):
+            # unlabel
+            start = i * self.n_batch_u
+            end = start + self.n_batch_u
+            batch_x_u = [_x[start:end] for _x in test_set_u]
+
+            # label
+            start = i * self.n_batch
+            end = start + self.n_batch
+            batch_x_l = [_x[start:end] for _x in test_set_l]
+
+            _x = batch_x_u + batch_x_l + [l, k]
+            lower_bound = self.lower_bound_test(*_x)
+            lower_bound_all.append(np.array(lower_bound))
+
+            if verbose:
+                pbar.update(i)
+
+        return lower_bound_all
+
+    def _discriminate(self, x, y, deterministic=False):
+        # log q(y|x0,...xn)
+        # _samples : [[x0,...,xn],y]
+        _samples = [x]+y
+        log_likelihood = self.f.log_likelihood_given_x(
+            _samples, deterministic=deterministic,
+            last_layer=True)
+        loss = -T.mean(log_likelihood)
+
+        return log_likelihood, loss
+
     def _elbo(self, x, l, annealing_beta, deterministic=False):
-        raise NotImplementedError
+        raise NotImplementedError        
 
     def _vr_bound(self, x, l=1, k=1,
                   iw_alpha=0, deterministic=False, y=None):
         if y is None:
+            supervised = False
+        else:
+            supervised = True
+
+        if supervised is False:
             # y ~ q(y|x)
             y = self.f.sample_given_x(x, repeat=l * k,
                                       deterministic=deterministic)[-1:]
@@ -46,12 +173,10 @@ class SS_JMVAE(VAE):
                                     deterministic=deterministic)[-1:]
         # q_samples = [z1,...,zL,s]
         q_samples = z+s
-        if y is None:
-            supervised = False
-        else:
-            supervised = True
+
+        # importance weighted
         log_iw = self._log_importance_weight(q_samples, y,
-                                             supervised=supervised
+                                             supervised=supervised,
                                              deterministic=deterministic)
 
         log_iw_matrix = log_iw.reshape((x[0].shape[0] * l, k))
@@ -66,12 +191,15 @@ class SS_JMVAE(VAE):
         for i, p in enumerate(self.p):
             p_params += self.p[i].get_params()
         s_q_params = self.s_q.get_params()
-        f_params = self.f.get_params()
-        params = f_params + s_q_params + p_params
+        params = s_q_params + p_params
+
+        if supervised is False:
+            params += self.f.get_params()
 
         if self.prior_mode == "MultiPrior":
             params += self.prior.get_params()
 
+        params = sorted(set(params), key=params.index)
         return log_likelihood, loss, params
 
     def _log_importance_weight(self, samples, y, supervised=True,
@@ -109,8 +237,8 @@ class SS_JMVAE(VAE):
 
         if supervised is False:
             # log q(y|x0,...xn)
-            # _samples : [[zL],y]
-            _samples = [tolist(samples[-2])]+y
+            # _samples : [[x0,...,xn],y]
+            _samples = [samples[0]]+y
             q_log_likelihood += self.f.log_likelihood_given_x(
                 _samples, deterministic=deterministic,
                 last_layer=True)
