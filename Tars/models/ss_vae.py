caec42d26f95f2ac5a84e89ac53a265cf309a91b
masa-su <masa@weblab.t.u-tokyo.ac.jp>
2017-07-30 00:45:02 +0900

    comment out

--- Tars/models/ss_vae.py
+++ Tars/models/ss_vae.py
@@ -342,7 +342,7 @@ class SS_VAE(VAE):
             log_iw += self.prior.log_likelihood_given_x(prior_samples)
         else:
             log_iw += self.prior.log_likelihood(prior_samples)
-        log_iw += 1. / np.float32(10)  # Categorical prior distribution
+#        log_iw += 1. / np.float32(10)  # Categorical prior distribution
 
         return log_iw
 
